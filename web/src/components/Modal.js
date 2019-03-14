9eb79933cac847197f6cc37d56637e8f258d12de
Luca Lischetti <luca@sirlisko.com>
2019-03-14 13:39:26 +0000

    fixed body scroll on modal open

--- web/src/components/Modal.js
+++ web/src/components/Modal.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { IoIosClose } from 'react-icons/io';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 const Modal = props => {
   const { isModalOpen, onClose, children } = props;
   if (!isModalOpen) {
     return false;
   }
+  disableBodyScroll(document.body);
+  const handleClose = () => {
+    enableBodyScroll(document.body);
+    onClose();
+  };
   return (
-    <div className="modal__backdrop" onClick={onClose}>
+    <div className="modal__backdrop" onClick={handleClose}>
       <div className="modal" onClick={e => e.stopPropagation()}>
-        <button className="modal__close" onClick={onClose}>
+        <button className="modal__close" onClick={handleClose}>
           <IoIosClose />
         </button>
         <div>{children}</div>
