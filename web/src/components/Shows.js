d9f2e843d8702a0be17da7f50fbbf43a0a041a00
Luca Lischetti <luca@sirlisko.com>
2019-05-13 17:32:02 +0100

    only show next shows if meaningful

--- web/src/components/Shows.js
+++ web/src/components/Shows.js
@@ -3,14 +3,17 @@ import React from 'react';
 import { nextShows, pastShows, showBlock } from '../helpers/shows';
 
 export default function Shows({ shows }) {
+  const next = nextShows(shows);
   return (
     <>
-      <div className="box box_4">
-        <div className="rotate">
-          <h2>Next shows</h2>
-          {nextShows(shows, 'rotate--alt').map(show => showBlock(show))}
+      {next.length > 0 && (
+        <div className="box box_4">
+          <div className="rotate">
+            <h2>Next shows</h2>
+            {next.map(show => showBlock(show))}
+          </div>
         </div>
-      </div>
+      )}
       <div className="box box_3">
         <div>
           <h2>Past shows</h2>
